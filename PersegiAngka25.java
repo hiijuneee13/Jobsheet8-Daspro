d707d54382697d63033388e1bd70ecc71cd67476
Rizal Maulana <xjunngoat1@gmail.com>
2025-10-30 08:21:24 +0700

    Perubahan code

--- PersegiAngka25.java
+++ PersegiAngka25.java
@@ -12,15 +12,7 @@ public class PersegiAngka25 {
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= n; j++) {
                 if (i == 1 || i == n || j == 1 || j == n) {
-                    System.out.print("3 ");
-                } else {
-                    System.out.print("  ");
-                }
-            }
-            System.out.print("   ");
-            for (int j = 1; j <= n; j++) {
-                if (i == 1 || i == n || j == 1 || j == n) {
-                    System.out.print("5 ");
+                    System.out.print(n + " ");
                 } else {
                     System.out.print("  ");
                 }
